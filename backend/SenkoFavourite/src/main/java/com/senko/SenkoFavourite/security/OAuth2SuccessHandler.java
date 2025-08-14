87b1eed986665ffb8f2ca1e55edda70038d76d6b
senkochi <senkosannanoja@gmail.com>
2025-08-14 15:37:45 +0700

    Remove sensitive config

---
+++ backend/SenkoFavourite/src/main/java/com/senko/SenkoFavourite/security/OAuth2SuccessHandler.java
@@ -0,0 +1,4 @@
+package com.senko.SenkoFavourite.security;
+
+public class OAuth2SuccessHandler {
+}
---
+++ backend/SenkoFavourite/src/main/java/com/senko/SenkoFavourite/service/CustomOAuth2UserService.java
@@ -0,0 +1,4 @@
+package com.senko.SenkoFavourite.service;
+
+public class CustomOAuth2UserService {
+}
